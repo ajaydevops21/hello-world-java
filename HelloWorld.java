be02d733f749f29250dc8c93434891d0f15415e6
ajaydevops21 <77962765+ajaydevops21@users.noreply.github.com>
2021-02-03 23:22:36 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -34,7 +34,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
-    System.out.println("Hello, Webhoos testing");
+    System.out.println("Hello, Webhooks testing");
   }
 }
